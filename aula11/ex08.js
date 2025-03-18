4e0259295bb3da831557df7fd0dd09651bf77679
Gabriel H. Silva <111374955+GabHSilva@users.noreply.github.com>
2025-03-18 06:17:43 -0300

    aula 11 concluida

---
+++ aula11/ex08.js
@@ -0,0 +1,8 @@
+var vel = 61
+console.log (`A sua velocidade atual é de ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log (`Sua velocidade esta acima do permitido, vc sera MULTADO`)
+}
+
+console.log (`Diriga com o cinto de segurança!`)
---
+++ aula11/ex09.js
@@ -0,0 +1,12 @@
+var país = 'EUA'
+var meup = 'Brasil'
+
+console.log (`Vivendo no ${país}`)
+
+if (meup != 'EUA') {
+    console.log(`vc é estrangeiro`)
+}
+
+else {
+    console.log (`vc é estadunidense`)
+}
